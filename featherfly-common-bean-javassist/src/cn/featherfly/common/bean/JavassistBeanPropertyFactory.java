4910ecb6f3752444ad60257b6e58026448f68f71
zhongji <zhongji@uwantsoft.com>
2015-06-28 18:57:34 +0800

    1.3.1

--- featherfly-common-bean-javassist/src/cn/featherfly/common/bean/JavassistBeanPropertyFactory.java
+++ featherfly-common-bean-javassist/src/cn/featherfly/common/bean/JavassistBeanPropertyFactory.java
@@ -7,7 +7,6 @@ import java.lang.reflect.Method;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import cn.featherfly.common.lang.ClassUtils;
 import cn.featherfly.common.lang.WordUtils;
 import javassist.ClassPool;
 import javassist.CtClass;
@@ -32,22 +31,16 @@ public class JavassistBeanPropertyFactory implements BeanPropertyFactory {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(JavassistBeanPropertyFactory.class);
 
-	/**
-	 *<p>
-	 * 动态创建指定类型指定属性对应的BeanProperty子类
-	 * </p>
-	 * @param type 类型
-	 * @param field 存取数据的字段
-	 * @param setMethod 设置方法
-	 * @param getMethod 读取方法
-	 * @return 动态创建的BeanProperty子类
-	 */
+	 /**
+     * {@inheritDoc}
+     */
 	@Override
-	public <T> BeanProperty<T> create(Class<?> type, Field field, Class<T> fieldType, Method setMethod, Method getMethod) {
+	public <T> BeanProperty<T> create(String propertyName, Field field, Class<T> propertyType, Method setMethod, Method getMethod
+            , Class<?> ownerType, Class<?> declaringType) {
 		try {
-			return create(type, field.getName(), field, fieldType, setMethod, getMethod);
+			return create(ownerType, propertyName, field, propertyType, setMethod, getMethod, declaringType);
 		} catch (Exception e) {
-			throw new NoSuchPropertyException(type, field.getName(), e);
+			throw new NoSuchPropertyException(ownerType, field.getName(), e);
 		}
 	}
 
@@ -84,29 +77,34 @@ public class JavassistBeanPropertyFactory implements BeanPropertyFactory {
 	 * @return BeanProperty
 	 */
 	@SuppressWarnings("unchecked")
-    private <T> BeanProperty<T> create(Class<?> type, String name, Field field, Class<T> fieldType, Method setMethod, Method getMethod) {
+    private <T> BeanProperty<T> create(Class<?> ownerType, String name, Field field, Class<T> propertyType, Method setMethod, Method getMethod, Class<?> declaringType) {
 		if (setMethod == null && getMethod == null) {
-			throw new NoSuchPropertyException(type, name, "没有读取和写入方法");
+			throw new NoSuchPropertyException(ownerType, name, "没有读取和写入方法");
 		}
 
 		try {
 			ClassPool pool = ClassPool.getDefault();
 
 			String upperCaseFirstName = WordUtils.upperCaseFirst(name);
-			String propertyClassName = type.getPackage() + "._" + type.getSimpleName()
+			String propertyClassName = ownerType.getPackage() + "._" + ownerType.getSimpleName()
 							+ upperCaseFirstName + "Property";
 			CtClass beanPropertyClass = pool.makeClass(propertyClassName);
 			beanPropertyClass.setSuperclass(pool.getCtClass(BeanProperty.class.getName()));
 
 			CtConstructor constraConstructor = new CtConstructor(
-					new CtClass[]{pool.getCtClass(Field.class.getName()),
+					new CtClass[]{
+					        pool.getCtClass(String.class.getName()),
+					        pool.getCtClass(Field.class.getName()),
+					        pool.getCtClass(Class.class.getName()),
 						pool.getCtClass(Method.class.getName()),
-						pool.getCtClass(Method.class.getName())
+						pool.getCtClass(Method.class.getName()),
+						pool.getCtClass(Class.class.getName()),
+						pool.getCtClass(Class.class.getName())
 					}, beanPropertyClass);
 			constraConstructor.setModifiers(Modifier.PUBLIC);
-			constraConstructor.setBody("super(" + type.getName() + ".class, $1, $2, $3);");
+			constraConstructor.setBody("super($1, $2, $3, $4, $5, $6, $7);");
 			beanPropertyClass.addConstructor(constraConstructor);
-
+						
 			CtClass objectType = pool.get(Object.class.getName());
 
 			String setterName = "set" + upperCaseFirstName;
@@ -117,15 +115,15 @@ public class JavassistBeanPropertyFactory implements BeanPropertyFactory {
 				setterBody = String.format("{"
 						+ "throw new " + PropertyAccessException.class.getName()
 						+ "(%s.class,\"%s\",\"属性不可写\");"
-						+ "}", type.getName(), name);
+						+ "}", ownerType.getName(), name);
 			} else {
 				setterBody = String.format("{"
 						+ "%1$s value = (%1$s) $2;"
 						+ "%2$s obj = (%2$s)$1;"
 						+ "obj.%3$s(value);"
-						+ "}", fieldType.getName(), type.getName(), setterName);
+						+ "}", propertyType.getName(), ownerType.getName(), setterName);
 			}
-			LOGGER.trace("类型{}属性{}的设置方法内容：{}", new Object[]{type.getName(), name, setterBody});
+			LOGGER.trace("类型{}属性{}的设置方法内容：{}", new Object[]{ownerType.getName(), name, setterBody});
 			setter.setBody(setterBody);
 			setter.setModifiers(Modifier.PUBLIC);
 			beanPropertyClass.addMethod(setter);
@@ -138,16 +136,16 @@ public class JavassistBeanPropertyFactory implements BeanPropertyFactory {
 				getterBody = String.format("{"
 					+ "throw new " + PropertyAccessException.class.getName()
 					+ "(%s.class,\"%s\",\"属性不可读\");"
-					+ "}", type.getName(), name);
+					+ "}", ownerType.getName(), name);
 			} else {
 				getterBody = String.format("{"
 						+ "%1$s obj = (%1$s)$1;"
 						+ "return obj.%2$s();"
-						+ "}", type.getName(), getterName);
+						+ "}", ownerType.getName(), getterName);
 			}
 
 
-			LOGGER.trace("类型{}属性{}的读取方法内容：{}", new Object[]{type.getName(), name, getterBody});
+			LOGGER.trace("类型{}属性{}的读取方法内容：{}", new Object[]{ownerType.getName(), name, getterBody});
 			getter.setBody(getterBody);
 			getter.setModifiers(Modifier.PUBLIC);
 			beanPropertyClass.addMethod(getter);
@@ -155,8 +153,9 @@ public class JavassistBeanPropertyFactory implements BeanPropertyFactory {
 			beanPropertyClass.toClass();
 			beanPropertyClass.detach();
 			return (BeanProperty<T>) Class.forName(propertyClassName)
-					.getConstructor(Field.class, Method.class, Method.class)
-					.newInstance(field, setMethod, getMethod);
+					.getConstructor(String.class, Field.class, Class.class, Method.class, Method.class
+					        , Class.class, Class.class)
+					.newInstance(name, field, propertyType, setMethod, getMethod, ownerType, declaringType);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}

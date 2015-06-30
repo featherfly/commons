f9a9393edc3ec0de4862be79cacf715a82bf9a58
zhongji <zhongji@uwantsoft.com>
2015-06-30 16:37:55 +0800

    加入日志

---
+++ featherfly-common/src-common/cn/featherfly/common/logging/LoggerLevel.java
@@ -0,0 +1,19 @@
+
+package cn.featherfly.common.logging;
+
+/**
+ * <p>
+ * LoggerLevel
+ * </p>
+ * 
+ * @author 钟冀
+ * @version 1.3
+ * @since 1.3
+ */
+public enum LoggerLevel {
+        ERROR,
+        WARN,
+        INFO,
+        DEBUG,
+        TRACE
+}

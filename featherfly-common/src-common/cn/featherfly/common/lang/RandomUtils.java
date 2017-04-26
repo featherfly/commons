d90612ebf1c546726fd8f16e204fa8f1c09c37bd
zhongji <zhongji@9imeihuo.com>
2017-04-26 18:05:07 +0800

    RandomUtils加入getRandomFromTotal(double total, int num)

--- featherfly-common/src-common/cn/featherfly/common/lang/RandomUtils.java
+++ featherfly-common/src-common/cn/featherfly/common/lang/RandomUtils.java
@@ -248,6 +248,50 @@ public final class RandomUtils {
         }
         return sb.toString();
     }
+    
+    /**
+     * 把传入总数分解为随机的传入份数
+     * @param total 总数
+     * @param num 分数
+     * @return 随机数组
+     */
+    public static double[] getRandomFromTotal(double total, int num) {
+        double[] results = new double[num];
+        LeftMoneyPackage leftMoneyPackage = new LeftMoneyPackage(total, num);
+        int i = 0;
+        while(leftMoneyPackage.remainSize > 0) {
+            results[i] = getRandomMoney(leftMoneyPackage);
+            i++;
+        }
+        return results;
+    }
+
+    private static double getRandomMoney(LeftMoneyPackage _leftMoneyPackage) {
+        if (_leftMoneyPackage.remainSize == 1) {
+            _leftMoneyPackage.remainSize--;
+            return (double) Math.round(_leftMoneyPackage.remainMoney * 100) / 100;
+        }
+        Random r     = new Random();
+        double min   = 0.01;
+        double max   = _leftMoneyPackage.remainMoney / _leftMoneyPackage.remainSize * 2;
+        double money = r.nextDouble() * max;
+        money = money <= min ? 0.01: money;
+        money = Math.floor(money * 100) / 100;
+        _leftMoneyPackage.remainSize--;
+        _leftMoneyPackage.remainMoney -= money;
+        return money;
+    }
+
+    private static class LeftMoneyPackage {
+        // remainSize 剩余的红包数量
+        private Integer remainSize;
+        // remainMoney 剩余的钱
+        private Double remainMoney;
+        public LeftMoneyPackage(Double remainMoney, Integer remainSize) {
+            this.remainSize = remainSize;
+            this.remainMoney = remainMoney;
+        }
+    }
 
     // ********************************************************************
     //    private method

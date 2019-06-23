452976263d51c0bb02701554c04d12a2b18bb25b
zhongj <featherfly@foxmail.com>
2019-06-23 16:51:21 +0800

    1.加入LocalizedCodeException,LocalizedExceptionCode,SimpleExceptionCode,SimpleLocalizedExceptionCode 2.ResourceBundleUtils加入getString(Class<?> type, String key, Object[] argus,Locale locale)等一系列方法 3.删除StandardResourceBundleException,StandardAppException,StandardConfigException,StandardSysException,         AssertStandardApp,AssertStandardConfig,AssertStandardSys

--- featherfly-common/src-common/cn/featherfly/common/exception/LocalizedException.java
+++ featherfly-common/src-common/cn/featherfly/common/exception/LocalizedException.java
@@ -11,7 +11,6 @@ import cn.featherfly.common.lang.ClassUtils;
 import cn.featherfly.common.lang.LogUtils;
 import cn.featherfly.common.locale.ResourceBundleUtils;
 
-
 /**
  * <p>
  * 支持国际化消息输出的异常
@@ -19,202 +18,294 @@ import cn.featherfly.common.locale.ResourceBundleUtils;
  *
  * @author 钟冀
  */
-public abstract class LocalizedException extends RuntimeException{
+public abstract class LocalizedException extends RuntimeException {
     /**
      * logger
      */
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
-    
+
     private static final long serialVersionUID = -580152334157640022L;
-    
+
     private ExceptionCode exceptionCode;
-    
+
     /**
      * 构造方法
      */
     protected LocalizedException() {
     }
+
     /**
      * 构造方法
-     * @param message 信息
-     * @param argus 消息绑定参数
-     * @param locale locale
-     * @param ex 异常
+     * 
+     * @param message
+     *            信息
+     * @param argus
+     *            消息绑定参数
+     * @param locale
+     *            locale
+     * @param ex
+     *            异常
      */
-    protected LocalizedException(String message, Object[] argus, Locale locale, Throwable ex) {
+    protected LocalizedException(String message, Object[] argus, Locale locale,
+            Throwable ex) {
         super(message, ex);
         setMessage(message, argus, locale);
     }
+
     /**
      * 构造方法
-     * @param message 信息
-     * @param locale locale
-     * @param ex 异常
+     * 
+     * @param message
+     *            信息
+     * @param locale
+     *            locale
+     * @param ex
+     *            异常
      */
     protected LocalizedException(String message, Locale locale, Throwable ex) {
-        this(message, new Object[]{}, locale, ex);
+        this(message, new Object[] {}, locale, ex);
     }
+
     /**
      * 构造方法
-     * @param message 信息
-     * @param argus 消息绑定参数
-     * @param ex 异常
+     * 
+     * @param message
+     *            信息
+     * @param argus
+     *            消息绑定参数
+     * @param ex
+     *            异常
      */
     protected LocalizedException(String message, Object[] argus, Throwable ex) {
         this(message, argus, ResourceBundleUtils.getLocale(), ex);
     }
-    
+
     /**
      * 构造方法
-     * @param message 信息
-     * @param ex 异常
+     * 
+     * @param message
+     *            信息
+     * @param ex
+     *            异常
      */
     protected LocalizedException(String message, Throwable ex) {
-        this(message, new Object[]{}, ex);
+        this(message, new Object[] {}, ex);
     }
+
     /**
      * 构造方法
-     * @param message 信息
-     * @param argus 消息绑定参数
-     * @param locale locale
+     * 
+     * @param message
+     *            信息
+     * @param argus
+     *            消息绑定参数
+     * @param locale
+     *            locale
      */
-    protected LocalizedException(String message, Object[] argus, Locale locale) {
+    protected LocalizedException(String message, Object[] argus,
+            Locale locale) {
         super(message);
         setMessage(message, argus, locale);
     }
+
     /**
      * 构造方法
-     * @param message 信息
-     * @param argus 消息绑定参数
+     * 
+     * @param message
+     *            信息
+     * @param argus
+     *            消息绑定参数
      */
     protected LocalizedException(String message, Object[] argus) {
         this(message, argus, ResourceBundleUtils.getLocale());
     }
+
     /**
      * 构造方法
-     * @param message 信息
-     * @param locale locale
+     * 
+     * @param message
+     *            信息
+     * @param locale
+     *            locale
      */
     protected LocalizedException(String message, Locale locale) {
-        this(message, new Object[]{}, locale);
-    }
-    /**
-     * 构造方法
-     * @param message 信息
-     */
-    protected LocalizedException(String message) {
-        this(message, new Object[]{});
+        this(message, new Object[] {}, locale);
     }
 
     /**
      * 构造方法
-     * @param ex 异常
+     * 
+     * @param message
+     *            信息
+     */
+    protected LocalizedException(String message) {
+        this(message, new Object[] {});
+    }
+
+    /**
+     * 构造方法
+     * 
+     * @param ex
+     *            异常
      */
     protected LocalizedException(Throwable ex) {
         super(ex);
     }
+
+    /**
+     * 构造方法
+     * 
+     * @param exceptionCode
+     *            错误码
+     */
+    protected LocalizedException(ExceptionCode exceptionCode) {
+        this(exceptionCode.getMessage());
+        this.exceptionCode = exceptionCode;
+    }
     
     /**
      * 构造方法
-     * @param exceptionCode 错误码
+     * 
+     * @param exceptionCode
+     *            错误码
+     * @param ex
+     *            异常
      */
-    protected LocalizedException(ExceptionCode exceptionCode) {
-        super(exceptionCode.getMessage());
+    protected LocalizedException(ExceptionCode exceptionCode, Throwable ex) {
+        this(exceptionCode.getMessage(), ex);
         this.exceptionCode = exceptionCode;
     }
 
     /**
      * 构造方法
-     * @param exceptionCode 错误码
-     * @param argus 信息绑定参数
-     * @param locale locale
-     * @param ex 异常
+     * 
+     * @param exceptionCode
+     *            错误码
+     * @param argus
+     *            信息绑定参数
+     * @param locale
+     *            locale
+     * @param ex
+     *            异常
+     * @deprecated {@link LocalizedCodeException#LocalizedCodeException(LocalizedExceptionCode, Throwable)}
      */
-    protected LocalizedException(ExceptionCode exceptionCode, Object[] argus, Locale locale, Throwable ex) {
+    @Deprecated
+    protected LocalizedException(ExceptionCode exceptionCode, Object[] argus,
+            Locale locale, Throwable ex) {
         this(exceptionCode.getMessage(), argus, locale, ex);
         this.exceptionCode = exceptionCode;
     }
+
     /**
      * 构造方法
-     * @param exceptionCode 错误码
-     * @param locale locale
-     * @param ex 异常
+     * 
+     * @param exceptionCode
+     *            错误码
+     * @param locale
+     *            locale
+     * @param ex
+     *            异常
+     * @deprecated {@link LocalizedCodeException#LocalizedCodeException(LocalizedExceptionCode, Throwable)}
      */
-    protected LocalizedException(ExceptionCode exceptionCode, Locale locale, Throwable ex) {
+    @Deprecated
+    protected LocalizedException(ExceptionCode exceptionCode, Locale locale,
+            Throwable ex) {
         this(exceptionCode.getMessage(), locale, ex);
         this.exceptionCode = exceptionCode;
     }
+
     /**
      * 构造方法
-     * @param argus 信息绑定参数
-     * @param exceptionCode 错误码
-     * @param ex 异常
+     * 
+     * @param argus
+     *            信息绑定参数
+     * @param exceptionCode
+     *            错误码
+     * @param ex
+     *            异常
+     * @deprecated {@link LocalizedCodeException#LocalizedCodeException(LocalizedExceptionCode, Throwable)}
      */
-    protected LocalizedException(ExceptionCode exceptionCode, Object[] argus, Throwable ex) {
+    @Deprecated
+    protected LocalizedException(ExceptionCode exceptionCode, Object[] argus,
+            Throwable ex) {
         this(exceptionCode.getMessage(), argus, ex);
         this.exceptionCode = exceptionCode;
     }
 
     /**
      * 构造方法
-     * @param exceptionCode 错误码
-     * @param ex 异常
+     * 
+     * @param argus
+     *            信息绑定参数
+     * @param locale
+     *            locale
+     * @param exceptionCode
+     *            错误码
+     * @deprecated {@link LocalizedCodeException#LocalizedCodeException(LocalizedExceptionCode, Throwable)}
      */
-    protected LocalizedException(ExceptionCode exceptionCode, Throwable ex) {
-        this(exceptionCode.getMessage(), ex);
-        this.exceptionCode = exceptionCode;
-    }
-    /**
-     * 构造方法
-     * @param argus 信息绑定参数
-     * @param locale locale
-     * @param exceptionCode 错误码
-     */
-    protected LocalizedException(ExceptionCode exceptionCode, Object[] argus, Locale locale) {
+    @Deprecated
+    protected LocalizedException(ExceptionCode exceptionCode, Object[] argus,
+            Locale locale) {
         this(exceptionCode.getMessage(), argus, locale);
         this.exceptionCode = exceptionCode;
     }
+
     /**
      * 构造方法
-     * @param argus 信息绑定参数
-     * @param exceptionCode 错误码
+     * 
+     * @param argus
+     *            信息绑定参数
+     * @param exceptionCode
+     *            错误码
+     * @deprecated {@link LocalizedCodeException#LocalizedCodeException(LocalizedExceptionCode)}
      */
+    @Deprecated
     protected LocalizedException(ExceptionCode exceptionCode, Object[] argus) {
         this(exceptionCode.getMessage(), argus);
         this.exceptionCode = exceptionCode;
     }
+
     /**
      * 构造方法
-     * @param locale locale
-     * @param exceptionCode 错误码
+     * 
+     * @param locale
+     *            locale
+     * @param exceptionCode
+     *            错误码
+     * @deprecated {@link LocalizedCodeException#LocalizedCodeException(LocalizedExceptionCode)}
      */
+    @Deprecated
     protected LocalizedException(ExceptionCode exceptionCode, Locale locale) {
         this(exceptionCode.getMessage(), locale);
         this.exceptionCode = exceptionCode;
     }
-    
+
     /**
      * 返回exceptionCode
+     * 
      * @return exceptionCode
      */
     public ExceptionCode getExceptionCode() {
         return exceptionCode;
     }
+
     private void setMessage(String message, Object[] argus, Locale locale) {
         String msg = null;
-        char keySign = ResourceBundleUtils.KEY_SIGN;
-        char resourceSign = ResourceBundleUtils.RESOURCE_SIGN;
-        int keyIndex = message.indexOf(keySign);
+        int keyIndex = message.indexOf(ResourceBundleUtils.KEY_SIGN);
         char firstChar = message.charAt(0);
-        if (firstChar == resourceSign && keyIndex != -1) {
+        if (firstChar == ResourceBundleUtils.RESOURCE_SIGN && keyIndex != -1) {
             msg = ResourceBundleUtils.getString(message, argus, locale);
-        } else if (firstChar == keySign) {
+        } else if (firstChar == ResourceBundleUtils.KEY_SIGN) {
             try {
                 msg = ResourceBundleUtils.getString(
-                        ResourceBundleUtils.RESOURCE_SIGN + this.getClass().getName() + message, argus, locale);
+                        ResourceBundleUtils.RESOURCE_SIGN
+                                + this.getClass().getName() + message,
+                        argus, locale);
             } catch (MissingResourceException e) {
                 msg = ResourceBundleUtils.getString(
-                        ResourceBundleUtils.RESOURCE_SIGN + this.getClass().getSimpleName() + message, argus, locale);                
+                        ResourceBundleUtils.RESOURCE_SIGN
+                                + this.getClass().getSimpleName() + message,
+                        argus, locale);
             }
         } else {
             msg = message;
@@ -224,7 +315,7 @@ public abstract class LocalizedException extends RuntimeException{
             f.setAccessible(true);
             f.set(this, msg);
             f.setAccessible(false);
-        } catch (Exception e) {        
+        } catch (Exception e) {
             LogUtils.error(e, logger);
         }
     }
--- featherfly-common/src-common/cn/featherfly/common/locale/ResourceBundleUtils.java
+++ featherfly-common/src-common/cn/featherfly/common/locale/ResourceBundleUtils.java
@@ -3,6 +3,7 @@ package cn.featherfly.common.locale;
 
 import java.text.MessageFormat;
 import java.util.Locale;
+import java.util.MissingResourceException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,17 +19,16 @@ import cn.featherfly.common.lang.ServiceLoaderUtils;
  * @author 钟冀
  */
 public final class ResourceBundleUtils {
-    
+
     private static final Logger LOGGER = LoggerFactory
             .getLogger(ResourceBundleUtils.class);
-    
-    private static final ResourceBundleFactory RESOURCE_BUNDLE_FACTORY = ServiceLoaderUtils.load(
-            ResourceBundleFactory.class, new JdkResourceBundleFactory());
-    
-    private static final LocaleManager LOCALE_MANAGER = ServiceLoaderUtils.load(
-            LocaleManager.class, new DefaultLocaleManager());
 
-    
+    private static final ResourceBundleFactory RESOURCE_BUNDLE_FACTORY = ServiceLoaderUtils
+            .load(ResourceBundleFactory.class, new JdkResourceBundleFactory());
+
+    private static final LocaleManager LOCALE_MANAGER = ServiceLoaderUtils
+            .load(LocaleManager.class, new DefaultLocaleManager());
+
     /**
      * 
      */
@@ -44,69 +44,89 @@ public final class ResourceBundleUtils {
      * key标识符
      */
     public static final char KEY_SIGN = '#';
-    
+
     /**
      * <p>
      * 获取ResourceBundleUtils在没有传入Locale时使用的默认Locale,默认Locale使用LocaleManager加载
      * </p>
+     * 
      * @return Locale
      */
     public static Locale getLocale() {
         return LOCALE_MANAGER.getLocale();
     }
-    
+
     /**
      * <p>
      * getBundle,use default locale
      * </p>
+     * 
      * @param baseName
-     *        the base name of the resource bundle, a fully qualified class name
+     *            the base name of the resource bundle, a fully qualified class
+     *            name
      * @return ResourceBundle
      */
     public static ResourceBundle getBundle(String baseName) {
-        return RESOURCE_BUNDLE_FACTORY.getBundle(baseName, LOCALE_MANAGER.getLocale());
+        return RESOURCE_BUNDLE_FACTORY.getBundle(baseName,
+                LOCALE_MANAGER.getLocale());
     }
+
     /**
      * <p>
      * getBundle
      * </p>
+     * 
      * @param baseName
-     *        the base name of the resource bundle, a fully qualified class name
+     *            the base name of the resource bundle, a fully qualified class
+     *            name
      * @param locale
-     *        the locale for which a resource bundle is desired
+     *            the locale for which a resource bundle is desired
      * @return ResourceBundle
      */
     public static ResourceBundle getBundle(String baseName, Locale locale) {
         return RESOURCE_BUNDLE_FACTORY.getBundle(baseName, locale);
     }
+
     /**
      * {@link java.util.ResourceBundle#getString(java.lang.String) getString}
+     * 
      * @see java.util.ResourceBundle#getString(java.lang.String) getString
-     * @param pattern 查找字符串的模式
+     * @param pattern
+     *            查找字符串的模式
      * @return the string for the given key
      */
     public static String getString(String pattern) {
         return getString(pattern, null);
     }
+
     /**
      * {@link java.util.ResourceBundle#getString(java.lang.String) getString}
+     * 
      * @see java.util.ResourceBundle#getString(java.lang.String) getString
-     * @param pattern 查找字符串的模式
-     * @param argus argus
+     * @param pattern
+     *            查找字符串的模式
+     * @param argus
+     *            string format argus
      * @return the string for the given key
      */
     public static String getString(String pattern, Object[] argus) {
-        return getString(pattern, argus, LOCALE_MANAGER.getLocale());                
+        return getString(pattern, argus, LOCALE_MANAGER.getLocale());
     }
+
     /**
      * {@link java.util.ResourceBundle#getString(java.lang.String) getString}
+     * 
      * @see java.util.ResourceBundle#getString(java.lang.String) getString
-     * @param pattern 查找字符串的模式
-     * @param argus argus
-     * @param locale locale
+     * @param pattern
+     *            查找字符串的模式
+     * @param argus
+     *            string format argus
+     * @param locale
+     *            locale
      * @return the string for the given key
      */
-    public static String getString(String pattern, Object[] argus, Locale locale) {
+    public static String getString(String pattern, Object[] argus,
+            Locale locale) {
         String baseName = null;
         String key = null;
         int keyIndex = pattern.indexOf(KEY_SIGN);
@@ -117,33 +137,121 @@ public final class ResourceBundleUtils {
             return getStringFromBundle(pattern, baseName, key, argus, locale);
         } else {
             throw new IllegalArgumentException(
-                "argu code must start with @ and split bundle baseName and key with #, example: @bundleBaseName#key");
+                    "argu code must start with @ and split bundle baseName and key with #, example: @bundleBaseName#key");
+        }
+    }
+
+    /**
+     * {@link java.util.ResourceBundle#getString(java.lang.String) getString}
+     * 
+     * @see java.util.ResourceBundle#getString(java.lang.String) getString
+     * @param baseName
+     *            bundle baseName
+     * @param key
+     *            string key
+     * @param argus
+     *            string format argus
+     * @param locale
+     *            locale
+     * @return the string for the given key
+     */
+    public static String getString(String baseName, String key, Object[] argus,
+            Locale locale) {
+        return getStringFromBundle(baseName, key, argus, locale);
+    }
+
+    /**
+     * {@link java.util.ResourceBundle#getString(java.lang.String) getString}
+     * 
+     * @see java.util.ResourceBundle#getString(java.lang.String) getString
+     * @param type
+     *            resolve for classType
+     * @param key
+     *            string key
+     * @return the string for the given key
+     */
+    public static String getString(Class<?> type, String key) {
+        return getString(type, key, getLocale());
+    }
+
+    /**
+     * {@link java.util.ResourceBundle#getString(java.lang.String) getString}
+     * 
+     * @see java.util.ResourceBundle#getString(java.lang.String) getString
+     * @param type
+     *            resolve for classType
+     * @param key
+     *            string key
+     * @param locale
+     *            locale
+     * @return the string for the given key
+     */
+    public static String getString(Class<?> type, String key, Locale locale) {
+        return getString(type, key, new Object[] {}, locale);
+    }
+
+    /**
+     * {@link java.util.ResourceBundle#getString(java.lang.String) getString}
+     * 
+     * @see java.util.ResourceBundle#getString(java.lang.String) getString
+     * @param type
+     *            resolve for classType
+     * @param key
+     *            string key
+     * @param argus
+     *            string format argus
+     * @return the string for the given key
+     */
+    public static String getString(Class<?> type, String key, Object[] argus) {
+        return getString(type, key, argus, getLocale());
+    }
+
+    /**
+     * {@link java.util.ResourceBundle#getString(java.lang.String) getString}
+     * 
+     * @see java.util.ResourceBundle#getString(java.lang.String) getString
+     * @param type
+     *            resolve for classType
+     * @param key
+     *            string key
+     * @param argus
+     *            string format argus
+     * @param locale
+     *            locale
+     * @return the string for the given key
+     */
+    public static String getString(Class<?> type, String key, Object[] argus,
+            Locale locale) {
+        try {
+            return getString(type.getName(), key,argus, locale);            
+        } catch (MissingResourceException e) {
+            return getString(type.getSimpleName(), key,argus, locale);
         }
     }
     
-    /**
-     * {@link java.util.ResourceBundle#getString(java.lang.String) getString}
-     * @see java.util.ResourceBundle#getString(java.lang.String) getString
-     * @param baseName bundle baseName
-     * @param key string key
-     * @param argus argus
-     * @param locale locale
-     * @return the string for the given key
-     */
-    public static String getString(String baseName, String key, Object[] argus, Locale locale) {
-        return format(
-                getBundle(baseName, locale).getString(key), argus);
-    }
-    
-    private static String getStringFromBundle(String code, String baseName, String key
-                    , Object[] argus, Locale locale) {
-        String string = format(
-                getBundle(baseName, locale).getString(key), argus);
-        LOGGER.debug("match ResourceBundle pattern -> [{}] : baseName[{}] and key[{}], message -> {}"
-                        , new Object[]{code, baseName, key, string});
+    private static String getStringFromBundle(String baseName,
+            String key, Object[] argus, Locale locale) {
+        if (locale == null) {
+            locale = getLocale();
+        }
+        String string = format(getBundle(baseName, locale).getString(key),
+                argus);
         return string;
     }
-    
+
+    private static String getStringFromBundle(String code, String baseName,
+            String key, Object[] argus, Locale locale) {
+        if (locale == null) {
+            locale = getLocale();
+        }
+        String string = format(getBundle(baseName, locale).getString(key),
+                argus);
+        LOGGER.debug(
+                "match ResourceBundle pattern -> [{}] : baseName[{}] and key[{}], message -> {}",
+                new Object[] { code, baseName, key, string });
+        return string;
+    }
+
     private static String format(String string, Object[] argus) {
         if (LangUtils.isNotEmpty(argus)) {
             return MessageFormat.format(string, argus);

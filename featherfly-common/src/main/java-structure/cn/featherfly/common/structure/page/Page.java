cb56e0d980b5fcd0a954e3c9b571aaa0dd7a124a
zhongji <zhongj@cdmhzx.com>
2019-06-28 17:20:48 +0800

    加入Page类

---
+++ featherfly-common/src/main/java-structure/cn/featherfly/common/structure/page/Page.java
@@ -0,0 +1,28 @@
+package cn.featherfly.common.structure.page;
+
+/**
+ * <p>
+ * 分页模型
+ * </p>
+ *
+ * @author zhongj
+ */
+public interface Page {
+    /**
+     * <p>
+     * 获取每页数量
+     * </p>
+     *
+     * @return 每页数量
+     */
+    Integer getPageSize();
+
+    /**
+     * <p>
+     * 获取页数
+     * </p>
+     *
+     * @return 页数
+     */
+    Integer getPageNumber();
+}
--- featherfly-common/src/main/java-structure/cn/featherfly/common/structure/page/Pagination.java
+++ featherfly-common/src/main/java-structure/cn/featherfly/common/structure/page/Pagination.java
@@ -1,7 +1,5 @@
 package cn.featherfly.common.structure.page;
 
-
-
 /**
  * <p>
  * 分页模型
@@ -9,59 +7,50 @@ package cn.featherfly.common.structure.page;
  *
  * @author zhongj
  */
-public interface Pagination {
-    /**
-     * <p>
-     * 获取每页数量
-     * </p>
-     * @return 每页数量
-     */
-    Integer getPageSize();
-    /**
-     * <p>
-     * 获取页数
-     * </p>
-     * @return 页数
-     */
-    Integer getPageNumber();
+public interface Pagination extends Page {
+
     /**
      * <p>
      * 获取总页数
      * </p>
+     *
      * @return 总页数
      */
     Integer getTotalPage();
+
     /**
      * <p>
      * 获取总数
      * </p>
+     *
      * @return 总数
      */
     Integer getTotal();
+
     /**
      * <p>
      * 设置每页数量
      * </p>
+     *
      * @param pageSize 每页数量
      */
     void setPageSize(Integer pageSize);
+
     /**
      * <p>
      * 设置页数
      * </p>
+     *
      * @param pageNumber 页数
      */
     void setPageNumber(Integer pageNumber);
+
     /**
      * <p>
      * 设置总数
      * </p>
+     *
      * @param total 总数
      */
     void setTotal(Integer total);
 }
-
-
-
-
-

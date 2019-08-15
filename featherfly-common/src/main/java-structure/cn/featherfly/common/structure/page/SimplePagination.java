4282cc42733833f49d6c85755223357119fecc3e
zhongji <zhongj@cdmhzx.com>
2019-08-15 17:10:13 +0800

    加入SimplePaginationResults,SimplePagination不再实现PaginationResults

--- featherfly-common/src/main/java-structure/cn/featherfly/common/structure/page/SimplePagination.java
+++ featherfly-common/src/main/java-structure/cn/featherfly/common/structure/page/SimplePagination.java
@@ -1,7 +1,5 @@
 package cn.featherfly.common.structure.page;
 
-import java.util.List;
-
 /**
  * <p>
  * 简单分页模型实现
@@ -10,7 +8,7 @@ import java.util.List;
  * @param <E> 存放的对象类型
  * @author zhongj
  */
-public class SimplePagination<E> implements PaginationResults<E> {
+public class SimplePagination implements Pagination {
 
     /**
      */
@@ -29,8 +27,8 @@ public class SimplePagination<E> implements PaginationResults<E> {
      * @param limit  limit
      */
     public SimplePagination(int offset, int limit) {
-        this.pageSize = limit;
-        this.pageNumber = (offset + limit) / limit;
+        pageSize = limit;
+        pageNumber = (offset + limit) / limit;
     }
 
     private Integer total;
@@ -38,8 +36,6 @@ public class SimplePagination<E> implements PaginationResults<E> {
     private Integer pageSize;
 
     private Integer pageNumber;
-
-    private List<E> pageResults;
 
     /**
      * <p>
@@ -61,14 +57,6 @@ public class SimplePagination<E> implements PaginationResults<E> {
      */
     public void setPageNumber(Integer pageNumber) {
         this.pageNumber = pageNumber;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void setPageResults(List<E> pageResults) {
-        this.pageResults = pageResults;
     }
 
     /**
@@ -113,68 +101,4 @@ public class SimplePagination<E> implements PaginationResults<E> {
     public Integer getPageNumber() {
         return pageNumber;
     }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public List<E> getPageResults() {
-        return pageResults;
-    }
-
-    /**
-     * 返回resultSize
-     *
-     * @return resultSize
-     */
-    @Override
-    public Integer getResultSize() {
-        if (pageResults != null) {
-            return pageResults.size();
-        } else {
-            return null;
-        }
-    }
-
-    //    /**
-    //     * {@inheritDoc}
-    //     */
-    //    @Override
-    //    public Pagination<E> next() {
-    //        return to(pageNumber + 1);
-    //    }
-    //
-    //    /**
-    //     * {@inheritDoc}
-    //     */
-    //    @Override
-    //    public Pagination<E> previous() {
-    //        return to(pageNumber - 1);
-    //    }
-    //
-    //    /**
-    //     * {@inheritDoc}
-    //     */
-    //    @Override
-    //    public Pagination<E> first() {
-    //        return to(1);
-    //    }
-    //
-    //    /**
-    //     * {@inheritDoc}
-    //     */
-    //    @Override
-    //    public Pagination<E> last() {
-    //        return to(getTotalPage());
-    //    }
-    //
-    //    /**
-    //     * {@inheritDoc}
-    //     */
-    //    @Override
-    //    public Pagination<E> to(Integer pageNumber) {
-    //        this.pageNumber = pageNumber;
-    ////        setPageResults(null);
-    //        return this;
-    //    }
 }
---
+++ featherfly-common/src/main/java-structure/cn/featherfly/common/structure/page/SimplePaginationResults.java
@@ -0,0 +1,67 @@
+package cn.featherfly.common.structure.page;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 带结果集的简单分页模型实现
+ * </p>
+ *
+ * @param <E> 存放的对象类型
+ * @author zhongj
+ */
+public class SimplePaginationResults<E> extends SimplePagination implements PaginationResults<E> {
+
+    /**
+     */
+    public SimplePaginationResults() {
+    }
+
+    /**
+     * @param limit limit
+     */
+    public SimplePaginationResults(Limit limit) {
+        super(limit);
+    }
+
+    /**
+     * @param offset offset
+     * @param limit  limit
+     */
+    public SimplePaginationResults(int offset, int limit) {
+        super(offset, limit);
+    }
+
+    private List<E> pageResults;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setPageResults(List<E> pageResults) {
+        this.pageResults = pageResults;
+    }
+
+    /**
+     * 返回resultSize
+     *
+     * @return resultSize
+     */
+    @Override
+    public Integer getResultSize() {
+        if (pageResults != null) {
+            return pageResults.size();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<E> getPageResults() {
+        return pageResults;
+    }
+
+}

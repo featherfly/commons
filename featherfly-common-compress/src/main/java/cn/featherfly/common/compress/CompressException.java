d2c26756e2d5a22f614d48bddb202ae2b6ca17b7
featherfly <featherfly@foxmail.com>
2020-02-17 15:06:41 +0800

    修复javadoc描述错误

--- featherfly-common-compress/src/main/java/cn/featherfly/common/compress/CompressException.java
+++ featherfly-common-compress/src/main/java/cn/featherfly/common/compress/CompressException.java
@@ -1,45 +1,52 @@
 
 package cn.featherfly.common.compress;
 
-
 /**
  * <p>
- * 应用服务器异常
+ * 压缩、解压异常
  * </p>
  * 
  * @author 钟冀
  */
-public class CompressException extends RuntimeException{
-	
-	private static final long serialVersionUID = 1L;
+public class CompressException extends RuntimeException {
 
-	/**
-	 */
-	public CompressException() {
-		super();
-	}
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 构造方法
-	 * @param msg 信息
-	 * @param ex 异常
-	 */
-	public CompressException(String msg, Throwable ex) {
-		super(msg, ex);
-	}
-	/**
-	 * 构造方法
-	 * @param msg 信息
-	 */
-	public CompressException(String msg) {
-		super(msg);
-	}
+    /**
+     */
+    public CompressException() {
+        super();
+    }
 
-	/**
-	 * 构造方法 
-	 * @param ex 异常
-	 */
-	public CompressException(Throwable ex) {
-		super(ex);
-	}
+    /**
+     * 构造方法
+     * 
+     * @param msg
+     *            信息
+     * @param ex
+     *            异常
+     */
+    public CompressException(String msg, Throwable ex) {
+        super(msg, ex);
+    }
+
+    /**
+     * 构造方法
+     * 
+     * @param msg
+     *            信息
+     */
+    public CompressException(String msg) {
+        super(msg);
+    }
+
+    /**
+     * 构造方法
+     * 
+     * @param ex
+     *            异常
+     */
+    public CompressException(Throwable ex) {
+        super(ex);
+    }
 }

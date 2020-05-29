9bd9f7a6db3c3a4e6e84685e4a0b71743f9eee93
zhongji <featherfly@foxmail.com>
2020-05-29 22:08:01 +0800

    common-api 0.1.0 release
    1.从core包迁移部分接口过来
    2.加入Logger,ArraySupplier,MapSupplier

---
+++ common-api/src/main/java/cn/featherfly/common/lang/function/ArraySupplier.java
@@ -0,0 +1,15 @@
+
+package cn.featherfly.common.lang.function;
+
+import java.util.function.Supplier;
+
+/**
+ * <p>
+ * ArraySupplier
+ * </p>
+ *
+ * @author zhongj
+ */
+public interface ArraySupplier<E> extends Supplier<E[]> {
+
+}
---
+++ common-api/src/main/java/cn/featherfly/common/lang/function/MapSupplier.java
@@ -0,0 +1,16 @@
+
+package cn.featherfly.common.lang.function;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+/**
+ * <p>
+ * ArraySupplier
+ * </p>
+ *
+ * @author zhongj
+ */
+public interface MapSupplier<K, V> extends Supplier<Map<K, V>> {
+
+}

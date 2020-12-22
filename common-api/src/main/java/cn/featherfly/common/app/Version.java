7ae53d3988ceb884091e8d7abab4c59dc7359035
zhongji <featherfly@foxmail.com>
2020-12-22 18:29:48 +0800

    add version

---
+++ common-api/src/main/java/cn/featherfly/common/app/Version.java
@@ -0,0 +1,24 @@
+
+package cn.featherfly.common.app;
+
+/**
+ * Version.
+ *
+ * @author zhongj
+ */
+public interface Version {
+
+    /**
+     * Gets the version.
+     *
+     * @return the version
+     */
+    long getVersion();
+
+    /**
+     * Gets the version name.
+     *
+     * @return the version name
+     */
+    String getVersionName();
+}

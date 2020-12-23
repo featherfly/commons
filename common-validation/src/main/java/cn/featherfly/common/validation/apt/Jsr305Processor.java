09519ef5a1fbf0c37bbf1c3563ac49d45c2987a1
zhongji <featherfly@foxmail.com>
2020-12-23 15:07:56 +0800

    实现Jsr305方法参数基于ast动态生成验证逻辑，抛出IllegalArgumentException异常

--- common-validation/src/main/java/cn/featherfly/common/validation/apt/Jsr305Processor.java
+++ common-validation/src/main/java/cn/featherfly/common/validation/apt/Jsr305Processor.java
@@ -31,46 +31,55 @@ public class Jsr305Processor extends JavacProcessor {
 
     @Override
     public void doInit(@Nonnull ProcessingEnvironment processingEnv) {
+        //        messager.printMessage(Diagnostic.Kind.NOTE, "SupportedOptions");
+        //        messager.printMessage(Diagnostic.Kind.NOTE, getSupportedOptions().toString());
+        //        messager.printMessage(Diagnostic.Kind.NOTE, "getSupportedAnnotationTypes");
+        //        messager.printMessage(Diagnostic.Kind.NOTE, getSupportedAnnotationTypes().toString());
+        //        messager.printMessage(Diagnostic.Kind.NOTE, "processingEnv.getOptions()");
+        //        messager.printMessage(Diagnostic.Kind.NOTE, processingEnv.getOptions().toString());
+    }
 
-        messager.printMessage(Diagnostic.Kind.NOTE, "SupportedOptions");
-        messager.printMessage(Diagnostic.Kind.NOTE, getSupportedOptions().toString());
-        messager.printMessage(Diagnostic.Kind.NOTE, "getSupportedAnnotationTypes");
-        messager.printMessage(Diagnostic.Kind.NOTE, getSupportedAnnotationTypes().toString());
-        messager.printMessage(Diagnostic.Kind.NOTE, "processingEnv.getOptions()");
-        messager.printMessage(Diagnostic.Kind.NOTE, processingEnv.getOptions().toString());
+    @Override
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+        final JavacElements elementUtils = (JavacElements) processingEnv.getElementUtils();
+        messager.printMessage(Diagnostic.Kind.NOTE, "start generate check for javax.annotation.Nonnull");
+        messager.printMessage(Diagnostic.Kind.NOTE,
+                "Nonull element size = " + roundEnvironment.getElementsAnnotatedWith(Nonnull.class).size());
+
+        for (Element element : roundEnvironment.getElementsAnnotatedWith(Nonnull.class)) {
+            if (element.getKind() == ElementKind.PARAMETER) {
+                JCMethodDecl jcMethodDecl = (JCMethodDecl) elementUtils.getTree(element.getEnclosingElement());
+                JCVariableDecl jcVariableDecl = (JCVariableDecl) elementUtils.getTree(element);
+                //                addImportInfo(element, IllegalArgumentException.class);
+                addAssertToBody(jcMethodDecl, jcVariableDecl);
+                //                throw new IllegalArgumentException(
+                //                        String.format("parameter [%s] can not be null", element.getSimpleName().toString()));
+            }
+            String name = element.getSimpleName().toString();
+            messager.printMessage(Diagnostic.Kind.NOTE, "Nonnull ->" + element.getKind() + "-" + name);
+        }
+
+        messager.printMessage(Diagnostic.Kind.NOTE, "end generate check for javax.annotation.Nonnull");
+        return true;
     }
 
     private void addAssertToBody(JCMethodDecl jcMethodDecl, JCVariableDecl jcVariableDecl) {
-        messager.printMessage(Diagnostic.Kind.NOTE, "addAssertToBody ");
-        messager.printMessage(Diagnostic.Kind.NOTE, jcMethodDecl.name.toString());
-        messager.printMessage(Diagnostic.Kind.NOTE, jcVariableDecl.name.toString());
-        messager.printMessage(Diagnostic.Kind.NOTE, jcMethodDecl.body.toString());
-
+        //        messager.printMessage(Diagnostic.Kind.NOTE, "addAssertToBody ");
+        //        messager.printMessage(Diagnostic.Kind.NOTE, jcMethodDecl.name.toString());
+        //        messager.printMessage(Diagnostic.Kind.NOTE, jcVariableDecl.name.toString());
+        //        messager.printMessage(Diagnostic.Kind.NOTE, jcMethodDecl.body.toString());
         String message = String.format("method %s args[%s] can not be null", jcMethodDecl.name.toString(),
                 jcVariableDecl.name.toString());
-
-        //        JCTree.JCExpression isEq = treeMaker.Binary(JCTree.Tag.EQ, treeMaker.Ident(jcVariableDecl),
-        //                treeMaker.Literal(TypeTag.BOT, null));
         JCTree.JCExpression isEq = javac.eqNull(jcVariableDecl);
-
-        //        JCStatement throwException = treeMaker
-        //                .Throw(treeMaker.NewClass(null, List.nil(),
-        //                        treeMaker.Ident(names.fromString(IllegalArgumentException.class.getSimpleName())),
-        //                        List.of(treeMaker.Literal(message)), null));
         JCStatement throwException = javac.throwException(IllegalArgumentException.class, message);
-
         JCIf jcIf = treeMaker.If(isEq, treeMaker.Block(0, List.of(throwException)), null);
-
         messager.printMessage(Diagnostic.Kind.NOTE, jcMethodDecl.body.getStatements().toString());
-
         List<JCTree.JCStatement> stats = List.of(jcIf);
         stats = stats.appendList(jcMethodDecl.body.getStatements());
         jcMethodDecl.body.stats = List.from(stats);
-
-        messager.printMessage(Diagnostic.Kind.NOTE, " ********************** ");
-        messager.printMessage(Diagnostic.Kind.NOTE, " ############### ");
-        messager.printMessage(Diagnostic.Kind.NOTE, jcMethodDecl.body.toString());
-
+        //        messager.printMessage(Diagnostic.Kind.NOTE, " ********************** ");
+        //        messager.printMessage(Diagnostic.Kind.NOTE, " ############### ");
+        //        messager.printMessage(Diagnostic.Kind.NOTE, jcMethodDecl.body.toString());
         //        jcMethodDecl.body = treeMaker
         //                .Block(0, List.of(
         //                        treeMaker.Exec(treeMaker.Apply(List.<JCTree.JCExpression>nil(),
@@ -81,36 +90,4 @@ public class Jsr305Processor extends JavacProcessor {
 
     }
 
-    @Override
-    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
-        final JavacElements elementUtils = (JavacElements) processingEnv.getElementUtils();
-        messager.printMessage(Diagnostic.Kind.NOTE, "start generate check");
-        messager.printMessage(Diagnostic.Kind.NOTE,
-                "Nonull element size = " + roundEnvironment.getElementsAnnotatedWith(Nonnull.class).size());
-
-        for (Element element : roundEnvironment.getElementsAnnotatedWith(Nonnull.class)) {
-            if (element.getKind() == ElementKind.PARAMETER) {
-                String methodName = element.getEnclosingElement().getSimpleName().toString();
-                String paramName = element.getSimpleName().toString();
-                JCMethodDecl jcMethodDecl = (JCMethodDecl) elementUtils.getTree(element.getEnclosingElement());
-                JCVariableDecl jcVariableDecl = (JCVariableDecl) elementUtils.getTree(element);
-
-                //                addImportInfo(element, IllegalArgumentException.class);
-
-                addAssertToBody(jcMethodDecl, jcVariableDecl);
-                //                throw new IllegalArgumentException(
-                //                        String.format("parameter [%s] can not be null", element.getSimpleName().toString()));
-            }
-
-            if (element.getKind() == ElementKind.PARAMETER || element.getKind() == ElementKind.CONSTRUCTOR) {
-
-            }
-
-            String name = element.getSimpleName().toString();
-            messager.printMessage(Diagnostic.Kind.NOTE, "Nonnull ->" + element.getKind() + "-" + name);
-        }
-
-        messager.printMessage(Diagnostic.Kind.NOTE, "end generate check");
-        return true;
-    }
 }

31e6de4defe122164422212e34b675b0861aec7e
zhongji <featherfly@foxmail.com>
2021-08-17 15:00:18 +0800

    加入Operator接口，QueryOperator,SortOperator,LogicOperator都实现该接口

---
+++ common-model-repository/src/main/java/cn/featherfly/common/repository/operate/Operator.java
@@ -0,0 +1,20 @@
+
+/*
+ * All rights Reserved, Designed By zhongj
+ * @Title: Operator.java
+ * @Package cn.featherfly.common.repository.operate
+ * @Description:Operator
+ * @author: zhongj
+ * @date: 2021-08-17 14:57:17
+ * @Copyright: 2021 www.featherfly.cn Inc. All rights reserved.
+ */
+package cn.featherfly.common.repository.operate;
+
+/**
+ * Operator.
+ *
+ * @author zhongj
+ */
+public interface Operator {
+
+}

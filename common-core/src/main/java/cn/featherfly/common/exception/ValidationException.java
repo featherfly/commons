5483fe77eaa2deccf100ea8c534ee4c64e45ea07
zhongji <featherfly@foxmail.com>
2021-11-30 19:48:28 +0800

    docs: 修复javadoc警告

--- common-core/src/main/java/cn/featherfly/common/exception/ValidationException.java
+++ common-core/src/main/java/cn/featherfly/common/exception/ValidationException.java
@@ -24,150 +24,184 @@ public class ValidationException extends LocalizedException {
     private static final long serialVersionUID = 5452458564880268259L;
 
     /**
-     *
+     * Instantiates a new validation exception.
      */
     public ValidationException() {
         super();
     }
 
     /**
-     * @param message
-     * @param locale
-     * @param ex
+     * Instantiates a new validation exception.
+     *
+     * @param message the message
+     * @param locale  the locale
+     * @param ex      the ex
      */
     public ValidationException(LocalizedMessage message, Locale locale, Throwable ex) {
         super(message, locale, ex);
     }
 
     /**
-     * @param message
-     * @param locale
+     * Instantiates a new validation exception.
+     *
+     * @param message the message
+     * @param locale  the locale
      */
     public ValidationException(LocalizedMessage message, Locale locale) {
         super(message, locale);
     }
 
     /**
-     * @param message
-     * @param args
-     * @param locale
-     * @param ex
+     * Instantiates a new validation exception.
+     *
+     * @param message the message
+     * @param args    the args
+     * @param locale  the locale
+     * @param ex      the ex
      */
     public ValidationException(LocalizedMessage message, Object[] args, Locale locale, Throwable ex) {
         super(message, args, locale, ex);
     }
 
     /**
-     * @param message
-     * @param args
-     * @param locale
+     * Instantiates a new validation exception.
+     *
+     * @param message the message
+     * @param args    the args
+     * @param locale  the locale
      */
     public ValidationException(LocalizedMessage message, Object[] args, Locale locale) {
         super(message, args, locale);
     }
 
     /**
-     * @param message
-     * @param args
-     * @param ex
+     * Instantiates a new validation exception.
+     *
+     * @param message the message
+     * @param args    the args
+     * @param ex      the ex
      */
     public ValidationException(LocalizedMessage message, Object[] args, Throwable ex) {
         super(message, args, ex);
     }
 
     /**
-     * @param message
-     * @param args
+     * Instantiates a new validation exception.
+     *
+     * @param message the message
+     * @param args    the args
      */
     public ValidationException(LocalizedMessage message, Object[] args) {
         super(message, args);
     }
 
     /**
-     * @param message
-     * @param ex
+     * Instantiates a new validation exception.
+     *
+     * @param message the message
+     * @param ex      the ex
      */
     public ValidationException(LocalizedMessage message, Throwable ex) {
         super(message, ex);
     }
 
     /**
-     * @param message
+     * Instantiates a new validation exception.
+     *
+     * @param message the message
      */
     public ValidationException(LocalizedMessage message) {
         super(message);
     }
 
     /**
-     * @param message
-     * @param locale
-     * @param ex
+     * Instantiates a new validation exception.
+     *
+     * @param message the message
+     * @param locale  the locale
+     * @param ex      the ex
      */
     public ValidationException(String message, Locale locale, Throwable ex) {
         super(message, locale, ex);
     }
 
     /**
-     * @param message
-     * @param locale
+     * Instantiates a new validation exception.
+     *
+     * @param message the message
+     * @param locale  the locale
      */
     public ValidationException(String message, Locale locale) {
         super(message, locale);
     }
 
     /**
-     * @param message
-     * @param args
-     * @param locale
-     * @param ex
+     * Instantiates a new validation exception.
+     *
+     * @param message the message
+     * @param args    the args
+     * @param locale  the locale
+     * @param ex      the ex
      */
     public ValidationException(String message, Object[] args, Locale locale, Throwable ex) {
         super(message, args, locale, ex);
     }
 
     /**
-     * @param message
-     * @param args
-     * @param locale
+     * Instantiates a new validation exception.
+     *
+     * @param message the message
+     * @param args    the args
+     * @param locale  the locale
      */
     public ValidationException(String message, Object[] args, Locale locale) {
         super(message, args, locale);
     }
 
     /**
-     * @param message
-     * @param args
-     * @param ex
+     * Instantiates a new validation exception.
+     *
+     * @param message the message
+     * @param args    the args
+     * @param ex      the ex
      */
     public ValidationException(String message, Object[] args, Throwable ex) {
         super(message, args, ex);
     }
 
     /**
-     * @param message
-     * @param args
+     * Instantiates a new validation exception.
+     *
+     * @param message the message
+     * @param args    the args
      */
     public ValidationException(String message, Object[] args) {
         super(message, args);
     }
 
     /**
-     * @param message
-     * @param ex
+     * Instantiates a new validation exception.
+     *
+     * @param message the message
+     * @param ex      the ex
      */
     public ValidationException(String message, Throwable ex) {
         super(message, ex);
     }
 
     /**
-     * @param message
+     * Instantiates a new validation exception.
+     *
+     * @param message the message
      */
     public ValidationException(String message) {
         super(message);
     }
 
     /**
-     * @param ex
+     * Instantiates a new validation exception.
+     *
+     * @param ex the ex
      */
     public ValidationException(Throwable ex) {
         super(ex);

4515210fb4f6d2e70cad2bc941d8aa040e5153d0
zhongji <featherfly@foxmail.com>
2021-12-19 19:07:28 +0800

    feat: 加入IgnorePolity

---
+++ common-model-repository/src/main/java/cn/featherfly/common/repository/IgnorePolicy.java
@@ -0,0 +1,35 @@
+package cn.featherfly.common.repository;
+
+import java.util.function.Predicate;
+
+import cn.featherfly.common.lang.Lang;
+
+public enum IgnorePolicy implements Predicate<Object> {
+    /**
+     * not ignore
+     */
+    NONE,
+    /**
+     * ignore null
+     */
+    NULL,
+    /**
+     * ignore null and empty (String Array Collection Map size = 0)
+     */
+    EMPTY;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean test(Object t) {
+        switch (this) {
+            case NULL:
+                return t == null;
+            case EMPTY:
+                return Lang.isEmpty(t);
+            default:
+                return false;
+        }
+    }
+}

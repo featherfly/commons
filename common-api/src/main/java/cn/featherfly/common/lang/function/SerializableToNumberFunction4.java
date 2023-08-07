dc7f91b7f4d0ff8f474679e65a6d4777f0bc46c9
zhongj <featherfly@foxmail.com>
2023-08-07 20:25:07 +0800

    feat: add SerializableToStringFunction1-9,SerializableToNumberFunction1-9

---
+++ common-api/src/main/java/cn/featherfly/common/lang/function/SerializableToNumberFunction4.java
@@ -0,0 +1,13 @@
+package cn.featherfly.common.lang.function;
+
+/**
+ * The Interface SerializableToNumberFunction.
+ *
+ * @author zhongj
+ * @param <T> the type of the input to the function
+ * @param <R> the type of the result of the function
+ */
+@FunctionalInterface
+public interface SerializableToNumberFunction4<T, R extends Number> extends SerializableToNumberFunction<T, R> {
+
+}
---
+++ common-api/src/main/java/cn/featherfly/common/lang/function/SerializableToStringFunction6.java
@@ -0,0 +1,12 @@
+package cn.featherfly.common.lang.function;
+
+/**
+ * The Interface SerializableToStringFunction.
+ *
+ * @author zhongj
+ * @param <T> the type of the input to the function
+ */
+@FunctionalInterface
+public interface SerializableToStringFunction6<T> extends SerializableToStringFunction<T> {
+
+}

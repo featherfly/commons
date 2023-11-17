cc46a0f47ac559a41dbbdc0e0093e3fd1e2f4ea9
zhongj <featherfly@foxmail.com>
2023-11-17 20:28:06 +0800

    feat: AliasField|Repository add getAliasOrName() method

--- common-model-repository/src/main/java/cn/featherfly/common/repository/AliasField.java
+++ common-model-repository/src/main/java/cn/featherfly/common/repository/AliasField.java
@@ -1,9 +1,20 @@
 package cn.featherfly.common.repository;
 
+import cn.featherfly.common.lang.Lang;
+
 /**
  * alias field.
  *
  * @author zhongj
  */
 public interface AliasField extends Field, Alias {
+
+    /**
+     * get alias or name. when alias is empty try to get name.
+     *
+     * @return the alias or name
+     */
+    default String getAliasOrName() {
+        return Lang.ifEmpty(alias(), this::name);
+    }
 }
--- common-model-repository/src/main/java/cn/featherfly/common/repository/AliasRepository.java
+++ common-model-repository/src/main/java/cn/featherfly/common/repository/AliasRepository.java
@@ -1,9 +1,20 @@
 package cn.featherfly.common.repository;
 
+import cn.featherfly.common.lang.Lang;
+
 /**
  * alias repository.
  *
  * @author zhongj
  */
 public interface AliasRepository extends Repository, Alias {
+
+    /**
+     * get alias or name. when alias is empty try to get name.
+     *
+     * @return the alias or name
+     */
+    default String getAliasOrName() {
+        return Lang.ifEmpty(alias(), this::name);
+    }
 }

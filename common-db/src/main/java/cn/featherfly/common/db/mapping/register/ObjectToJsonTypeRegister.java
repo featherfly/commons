a06bb184f1541c729767b1e8824fdac4ae58ed83
zhongj <featherfly@foxmail.com>
2025-12-10 15:38:01 +0800

    feat: ObjectToJsonTypeRegister add ObjectToJsonTypeRegister(Class) constructor

--- common-db/src/main/java/cn/featherfly/common/db/mapping/register/ObjectToJsonTypeRegister.java
+++ common-db/src/main/java/cn/featherfly/common/db/mapping/register/ObjectToJsonTypeRegister.java
@@ -14,16 +14,25 @@ import cn.featherfly.common.db.mapping.SqlTypeToJavaRegister;
  * @param <E> the element type
  */
 public class ObjectToJsonTypeRegister<E extends Serializable>
-        implements SqlTypeToJavaRegister<E>, JavaToSqlTypeRegister<E> {
+    implements SqlTypeToJavaRegister<E>, JavaToSqlTypeRegister<E> {
 
-    private Class<E> javaType;
+    private final Class<E> javaType;
 
-    private boolean storeAsString = true;
+    private final boolean storeAsString;
 
     /**
      * Instantiates a new object to json type register.
      *
-     * @param javaType      the java type
+     * @param javaType the java type
+     */
+    public ObjectToJsonTypeRegister(Class<E> javaType) {
+        this(javaType, true);
+    }
+
+    /**
+     * Instantiates a new object to json type register.
+     *
+     * @param javaType the java type
      * @param storeAsString the store as string
      */
     public ObjectToJsonTypeRegister(Class<E> javaType, boolean storeAsString) {
